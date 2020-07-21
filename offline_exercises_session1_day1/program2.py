69713e36bf156c91d08ab6029a78dcccf21715bf
spoorthyvv <spoorthyvv@gmail.com>
2020-07-21 12:37:49 +0530

    Day_1

---
+++ offline_exercises_session1_day1/program2.py
@@ -0,0 +1,12 @@
+# Python code to convert Celsius scale 
+# to Fahrenheit scale 
+def Cel_To_Fah(n): 
+      
+    # Used the formula 
+    return (n*1.8)+32
+
+
+
+n = int(input("Enter the value of centegrade"))
+print(int(Cel_To_Fah(n)))
+
